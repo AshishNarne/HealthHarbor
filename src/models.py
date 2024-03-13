ddd459e0a7d92b5efd7b57372d6f1342c9a6f212
blahnikkai <83978042+blahnikkai@users.noreply.github.com>
2024-03-13 18:25:26 -0400

    Split Doctor and Patient user types, not yet functional

--- src/models.py
+++ src/models.py
@@ -1,8 +1,10 @@
 from flask_login import UserMixin
 from datetime import datetime
+from enum import Enum, auto
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from . import db
+
 
 class User(UserMixin, db.Model):
     # id is just an autoincrementing(?) integer (1, 2, 3, ...) that uniquely identifies a user
@@ -12,8 +14,31 @@ class User(UserMixin, db.Model):
     fname: Mapped[str] = mapped_column()
     lname: Mapped[str] = mapped_column()
     pwd_hash: Mapped[str] = mapped_column()
+    user_type: Mapped[str] = mapped_column()
     reminders: Mapped[list['Reminder']] = relationship(back_populates='user')
-    
+    __mapper_args__ = {
+        'polymorphic_identity': 'employee',
+        'polymorphic_on': 'user_type',
+    }
+
+
+class Doctor(User):
+    id: Mapped[int] = mapped_column(ForeignKey('user.id'), primary_key=True)
+    patients: Mapped[list['Patient']] = relationship(back_populates='doctor', foreign_keys='Patient.doctor_id')
+    __mapper_args__ = {
+        'polymorphic_identity': 'doctor',
+    }
+
+
+class Patient(User):
+    id: Mapped[int] = mapped_column(ForeignKey('user.id'), primary_key=True)
+    doctor_id: Mapped[int] = mapped_column(ForeignKey('doctor.id'))
+    doctor: Mapped['Doctor'] = relationship(back_populates='patients', foreign_keys='Patient.doctor_id')
+    __mapper_args__ = {
+        'polymorphic_identity': 'patient',
+    }
+
+
 class Reminder(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
